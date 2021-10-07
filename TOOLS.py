37a7d3228cd2bb3b705fbac444b08adc8f877fb4
Aldi ❤️ <89829276+Aldi098@users.noreply.github.com>
2021-10-07 16:55:39 +0700

    Update TOOLS.py

--- TOOLS.py
+++ TOOLS.py
@@ -1,4 +1,4 @@
-# Encrypted by K-fuscator
+# Encrypted by ALDI098
 # Github- https://github.com/Aldi098
 
 exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
